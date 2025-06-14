9f7fe9dcf119beaaac2e9951c38465e20d97c3d4
Tuzinho <arthur-filho@live.com>
2025-06-14 13:52:21 -0300

    Implementação_teste atualizada

--- Implementaçao_teste_GNSS/Implementacao_teste/hdl/Acquisition.vhd
+++ Implementaçao_teste_GNSS/Implementacao_teste/hdl/Acquisition.vhd
@@ -36,12 +36,12 @@ architecture architecture_Acquisition of Acquisition is
    -- signal, component etc. declarations
 	signal Frequency_offset_data : std_logic_vector(9 downto 0); -- example
 	signal cos_signal, sin_signal : std_logic_vector(data_width-1 downto 0) ; -- example
-	signal I1_signal, Q1_signal, I2_signal, Q2_signal : std_logic_vector(data_width downto 0) ; -- example
-	signal FFT_I_signal, FFT_Q_signal, FFT_X_signal, FFT_Y_signal : std_logic_vector(24 downto 0) ; -- example
-    signal IFFT_out_real, IFFT_out_imag : std_logic_vector(31 downto 0); -- Verificar
-    signal IFFT_in_real, IFFT_in_imag : std_logic_vector(23 downto 0); -- Verificar
-    --singal CA_CONJ_real, CA_CONJ_imag : std_logic_vector(31 downto 0); -- Verificar
-
+	signal I1_signal, Q1_signal, I2_signal, Q2_signal : std_logic_vector(data_width downto 0); -- example
+	signal FFT_I_signal, FFT_Q_signal, FFT_X_signal, FFT_Y_signal : std_logic_vector(24 downto 0); -- example
+    --signal FFT_CA_in_real, FFT_CA_out_real, FFT_CA_in_imag, FFT_CA_out_imag : std_logic_vector ();
+    signal FFT_CA_CONJ_real, FFT_CA_CONJ_imag : std_logic_vector(31 downto 0); -- Verificar
+    signal IFFT_in_real, IFFT_in_imag, IFFT_out_real, IFFT_out_imag : std_logic_vector(31 downto 0); -- Verificar
+    
     component COREDDS_C0 is
     -- Port list
     port(
@@ -80,6 +80,22 @@ architecture architecture_Acquisition of Acquisition is
         );
     end component;
     
+    component complex_multiplier_C0 is
+    -- Port list
+    port(
+        --Inputs
+        aimag_i  : in  std_logic_vector(15 downto 0);
+        areal_i  : in  std_logic_vector(15 downto 0);
+        bimag_i  : in  std_logic_vector(15 downto 0);
+        breal_i  : in  std_logic_vector(15 downto 0);
+        clock_i  : in  std_logic;
+        nreset_i : in  std_logic;
+        
+        --Outputs
+        cimag_o  : out std_logic_vector(32 downto 0);
+        creal_o  : out std_logic_vector(32 downto 0)
+    );
+    
     component Multiplier_simplified is
     generic(
         data_width : integer := 12
@@ -93,17 +109,6 @@ architecture architecture_Acquisition of Acquisition is
         S :	out std_logic_vector(data_width downto 0)
     );
     end component;
-    
-    component complex_multiplier_C0 is
-    generic(
-        data_width : integer := 32
-    );
-    port(
-        --Inputs
-        
-        --Outputs
-        
-    );
     
     component UAL is
     generic(
@@ -129,9 +134,9 @@ begin
    MULT4: Multiplier_simplified generic map(data_width) port map(cos_signal,MAX_INPUT_Q,I2_signal);
    SUM_I: UAL generic map(data_width) port map(I1_signal,Q2_signal,'0',FFT_I_signal(data_width downto 0),FFT_I_signal(23));
    SUM_Q: UAL generic map(data_width) port map(I2_signal,Q1_signal,'0',FFT_Q_signal(data_width downto 0),FFT_Q_signal(23));
-   FFT_IQ: COREFFT_C0 port map(clk,FFT_Q_signal,FFT_I_signal,'1','1','1','1','0',open,FFT_X_signal,FFT_Y_signal,open,open);
-   
-   IFFT_IQ: COREFFT_C0 port map(clk, FFT_X_signal, FFT_Y_signal,'1','1','1','1','0',open,IFFT_out_real,IFFT_out_imag,open,open); -- Verificar
-   
+   FFT_IQ: COREFFT_C0 port map(CLK,FFT_Q_signal,FFT_I_signal,'1','1','1','1','0',open,FFT_X_signal,FFT_Y_signal,open,open);
+   FFT_CA: 
+   MULT5: complex_multiplier_C0 port map (FFT_X_signal, FFT_Y_signal, FFT_CA_CONJ_imag, FFT_CA_CONJ_real, "CLK", "RST", IFFT_in_imag, IFFT_in_real); -- Verificar
+   IFFT: COREFFT_C0 port map(CLK, IFFT_in_imag, IFFT_in_real,'1','1','1','1','1',open,IFFT_out_imag,IFFT_out_real,open,open); -- Verificar
    
 end architecture_Acquisition;
