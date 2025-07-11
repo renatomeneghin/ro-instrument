3367c90027dca3b260c67ce05a8ebd50459879fb
Tuzinho <arthur-filho@live.com>
2025-07-11 15:27:32 -0300

    Implementação_teste updated

--- Implementaçao_teste_GNSS/Implementacao_teste/hdl/Acquisition.vhd
+++ Implementaçao_teste_GNSS/Implementacao_teste/hdl/Acquisition.vhd
@@ -38,13 +38,21 @@ architecture architecture_Acquisition of Acquisition is
 	signal cos_signal, sin_signal : std_logic_vector(data_width-1 downto 0) ; -- example
 	signal I1_signal, Q1_signal, I2_signal, Q2_signal : std_logic_vector(data_width downto 0); -- example
 	signal FFT_I_signal, FFT_Q_signal, FFT_X_signal, FFT_Y_signal : std_logic_vector(24 downto 0); -- example
-    signal FFT_CA_in_real, FFT_CA_out_real, FFT_CA_in_imag, FFT_CA_out_imag : std_logic_vector (23 downto 0); -- Verificar
-    signal CA_CONJ_out_imag : std_logic_vector (23 downto 0); -- Verificar
-    signal mult_out_real, mult_out_imag : std_logic_vector(32 downto 0); -- Verificar
-    signal IFFT_in_real_temp, IFFT_in_imag_temp : std_logic_vector(31 downto 0); -- Verificar
-    signal IFFT_out_real, IFFT_out_imag : std_logic_vector(31 downto 0); -- Verificar
-    signal final_out_real, final_out_imag : std_logic_vector(23 downto 0); -- Verificar
-    signal final_out_real_scaled, final_out_imag_scaled : std_logic_vector(23 downto 0); -- Verificar
+    -- Verificar
+    signal FFT_CA_in_real, FFT_CA_out_real, FFT_CA_in_imag, FFT_CA_out_imag : std_logic_vector (23 downto 0); 
+    signal CA_CONJ_out_imag : std_logic_vector (23 downto 0); 
+    signal mult_out_real, mult_out_imag : std_logic_vector(32 downto 0);
+    signal IFFT_in_real_temp, IFFT_in_imag_temp : std_logic_vector(31 downto 0);
+    signal IFFT_out_real, IFFT_out_imag : std_logic_vector(31 downto 0);
+    -- Verificar
+    signal ca_bit : std_logic;
+    signal ca_bit_valid : std_logic;
+    signal ca_mem_bit : std_logic;
+    signal ca_counter : unsigned(9 downto 0);
+    signal ca_reset_counter : std_logic;
+    signal mem_addr_reg : unsigned(31 downto 0);
+    signal ca_bit_buffer : std_logic_vector(31 downto 0);
+    signal bit_index : integer range 0 to 31;  
     
     CA_CONJ_out_imag <= std_logic_vector(-signed(FFT_CA_out_imag));
     
@@ -150,6 +158,24 @@ architecture architecture_Acquisition of Acquisition is
     );
     end component;
     
+    component LFSR_generator is
+    generic(
+        WIDTH : integer := 10;
+        WIDTH_CMP : integer := 10;
+    );
+    port(
+        rst	: in STD_LOGIC;									-- reset
+		seed	: in STD_LOGIC_VECTOR (WIDTH-1 downto 0);	-- initial state
+		tap	: in STD_LOGIC_VECTOR (WIDTH-1 downto 0);	-- XOR taps for input
+		RESET : in STD_LOGIC_VECTOR (WIDTH-1 downto 0); -- reset at this state
+		output : in STD_LOGIC_VECTOR (WIDTH-1 downto 0);-- phase selector taps		
+		SEQ : out STD_LOGIC;							-- 1 bit output
+		count_cmp : in STD_LOGIC_VECTOR (WIDTH_CMP-1 downto 0); -- reset after X clocks
+		ENABLE : in STD_LOGIC;								-- Enable high (to freeze the LSFR)
+		valid : out STD_LOGIC;
+    );
+    end component;
+    
 begin
 
     -- architecture body
@@ -161,8 +187,10 @@ begin
     SUM_I: UAL generic map(data_width) port map(I1_signal,Q2_signal,'0',FFT_I_signal(data_width downto 0),FFT_I_signal(23));
     SUM_Q: UAL generic map(data_width) port map(I2_signal,Q1_signal,'0',FFT_Q_signal(data_width downto 0),FFT_Q_signal(23));
     FFT_IQ: COREFFT_C0 port map(CLK,FFT_Q_signal,FFT_I_signal,'1','1','1','1',open,FFT_X_signal,FFT_Y_signal,open,open);
+    --CA_LFSR: LFSR_generator generic map(data_width, WIDTH_CMP) port map(); -- Verificar
     FFT_CA: COREFFT_C1 port map(CLK,FFT_CA_in_imag,FFT_CA_in_real,'1','1','1','1',open,FFT_CA_out_imag,FFT_CA_out_real,open,open); -- Verificar
-    MULT5: complex_multiplier_C0 port map (FFT_X_signal, FFT_Y_signal, CA_CONJ_out_imag, FFT_CA_out_real, "CLK", "RST", IFFT_in_imag, IFFT_in_real); -- Verificar
+    MULT5: complex_multiplier_C0 port map (FFT_X_signal, FFT_Y_signal, CA_CONJ_out_imag, FFT_CA_out_real, CLK, '1', IFFT_in_imag, IFFT_in_real); -- Verificar
     IFFT: COREFFT_C0 port map(CLK, IFFT_in_imag, IFFT_in_real,'1','1','1','1',open,IFFT_out_imag,IFFT_out_real,open,open); -- Verificar
-   
+    
+    
 end architecture_Acquisition;
