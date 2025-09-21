73f227a0ab4d30446d04b322af3446fb9388c252
Tuzinho <arthur-filho@live.com>
2025-09-21 09:48:51 -0300

    update

--- Implementaçao_teste_GNSS/hdl/Counter_DDS_CA.vhd
+++ Implementaçao_teste_GNSS/hdl/Counter_DDS_CA.vhd
@@ -6,25 +6,25 @@ entity Counter_DDS_CA is
 port (
    clk      : in std_logic;
    reset    : in std_logic;
-   step     : in unsigned(15 downto 0);
-   freq_DDS : out unsigned(15 downto 7);
-   freq_CA  : out unsigned(7 downto 0)
+   inc      : in unsigned(15 downto 0);
+   freq_CA  : out unsigned(15 downto 7);
+   freq_DDS : out unsigned(7 downto 0)
 );
 end Counter_DDS_CA;
 
 architecture architecture_Counter_DDS_CA of Counter_DDS_CA is
-    signal acc : unsigned(15 downto 0);
+    signal count : unsigned(15 downto 0);
     
 begin
     process(clk, reset) is
     begin
         if reset = '1' then
-            acc <= (others => '0');
-        elsif clk'event and clk = '1' then 
-            acc <= acc + step;
+            count <= (others => '0');
+        elsif rising_edge(clk) then 
+            count <= count + inc;
         end if;
     end process;
-    freq_DDS <= acc(15 downto 7));
-    freq_CA  <= acc(7 downto 0);
+    freq_CA  <= count(15 downto 7);
+    freq_DDS <= count(7 downto 0);
     
 end architecture_Counter_DDS_CA;
