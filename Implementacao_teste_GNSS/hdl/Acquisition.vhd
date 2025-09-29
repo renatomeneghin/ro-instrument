0bdc10a01f6aceb1863e42dd8c301848aa88038c
renatomeneghin <142346494+renatomeneghin@users.noreply.github.com>
2025-09-28 22:57:15 -0300

    Ajustes no Acquisition

--- Implementacao_teste_GNSS/hdl/Acquisition.vhd
+++ Implementacao_teste_GNSS/hdl/Acquisition.vhd
@@ -22,9 +22,6 @@ use ieee.numeric_std.all;
 use work.all;
 
 entity Acquisition is
-generic (
-    data_width : integer := 12
-);
 port (
     --<port_name> : <direction> <type>;
 	CLK : IN  std_logic; -- example
@@ -40,33 +37,38 @@ end Acquisition;
 
 architecture architecture_Acquisition of Acquisition is
     -- signal, component etc. declarations
+    constant Contador_WIDTH     : integer := 10;
+    constant DDS_Width          : integer := 8; -- Datawidth of the DDS
+    constant SUM_Width          : integer := 9; -- Datawidth of the summer before the FFT
+    constant FFT_Width          : integer := 10; -- Datawidth before the fft
+    constant IFFT_Width         : integer := 17; -- Datawidth before the ifft
+    
     -- slower clk
     signal clk_div4, slw_clk : std_logic;
     signal NRST : std_logic;
    
+    -- Controle do processo
+    signal count_state  : std_logic_vector(Contador_WIDTH-1 downto 0); -- example
+	signal Frequency_offset_data : std_logic_vector(Contador_WIDTH-6 downto 0); -- example
+    
     -- Entrada do sinal 
-	signal Frequency_offset_data : std_logic_vector(9 downto 0); -- example
-	signal cos_signal, sin_signal : std_logic_vector(data_width-1 downto 0) ; -- example
-	signal I1_signal, Q1_signal, I2_signal, Q2_signal, Q2_signal_n: std_logic_vector(data_width downto 0); -- example
-	signal FFT_I_signal, FFT_Q_signal, FFT_X_signal, FFT_Y_signal : std_logic_vector(9 downto 0); -- example
+    signal cos_signal, sin_signal : std_logic_vector(DDS_Width-1 downto 0) ; -- example
+	signal I1_signal, Q1_signal, I2_signal, Q2_signal, Q2_signal_n: std_logic_vector(SUM_Width-1 downto 0); -- example
+	signal FFT_I_signal, FFT_Q_signal, FFT_X_signal, FFT_Y_signal : std_logic_vector(FFT_Width-1 downto 0); -- example
+    
     -- R�plica sinal C/A
     signal ca_prn, ca_clk, ca_rst : std_logic;
     signal ca_enable, ca_valid : std_logic;
     signal ca_epoch, counter_init : std_logic;
     signal ca_counter : std_logic_vector(5 downto 0);
     signal sat_int: integer range 0 to 31; -- 32 GPS
-    signal FFT_CA_in_real, FFT_CA_out_real, FFT_CA_in_imag, FFT_CA_out_imag : std_logic_vector (9 downto 0); 
-    signal CA_CONJ_out_imag : std_logic_vector (9 downto 0); 
+    signal FFT_CA_in_real, FFT_CA_out_real, FFT_CA_in_imag, FFT_CA_out_imag : std_logic_vector (FFT_Width-1 downto 0); 
+    signal CA_CONJ_out_imag : std_logic_vector (FFT_Width-1 downto 0); 
+    --signal CA_NEG_out_imag : std_logic_vector (9 downto 0); 
     
     -- Sinais transformados
-    signal IFFT_in_imag, IFFT_in_real : std_logic_vector (20 downto 0); 
-    
-    signal count_state: std_logic_vector(9 downto 0); 
-    signal ca_reset_counter : std_logic;
-    signal mem_addr_reg : std_logic_vector(31 downto 0);
-    signal ca_bit_buffer : std_logic_vector(31 downto 0);
-    signal bit_index : integer range 0 to 31;  
-    
+    signal IFFT_in_imag, IFFT_in_real : std_logic_vector (IFFT_Width-1 downto 0); 
+        
     component PF_CLK_DIV_C0 is
     port(
     -- Inputs
@@ -96,9 +98,9 @@ architecture architecture_Acquisition of Acquisition is
         NGRST          : in  std_logic;
         RSTN           : in  std_logic;
         -- Outputs
-        COSINE         : out std_logic_vector(6 downto 0);
+        COSINE         : out std_logic_vector(DDS_Width-1 downto 0);
         INIT_OVER      : out std_logic;
-        SINE           : out std_logic_vector(6 downto 0)
+        SINE           : out std_logic_vector(DDS_Width-1 downto 0)
         );
     end component;
     
@@ -106,16 +108,16 @@ architecture architecture_Acquisition of Acquisition is
     port(
         -- Inputs
         CLK         : in  std_logic;
-        DATAI_IM    : in  std_logic_vector(7 downto 0);
-        DATAI_RE    : in  std_logic_vector(7 downto 0);
+        DATAI_IM    : in  std_logic_vector(FFT_Width-1 downto 0);
+        DATAI_RE    : in  std_logic_vector(FFT_Width-1 downto 0);
         DATAI_VALID : in  std_logic;
         NGRST       : in  std_logic;
         READ_OUTP   : in  std_logic;
         SLOWCLK     : in  std_logic;
         -- Outputs
         BUF_READY   : out std_logic;
-        DATAO_IM    : out std_logic_vector(7 downto 0);
-        DATAO_RE    : out std_logic_vector(7 downto 0);
+        DATAO_IM    : out std_logic_vector(FFT_Width-1 downto 0);
+        DATAO_RE    : out std_logic_vector(FFT_Width-1 downto 0);
         DATAO_VALID : out std_logic;
         OUTP_READY  : out std_logic
     );
@@ -125,16 +127,16 @@ architecture architecture_Acquisition of Acquisition is
     port(
         -- Inputs
         CLK         : in  std_logic;
-        DATAI_IM    : in  std_logic_vector(23 downto 0);
-        DATAI_RE    : in  std_logic_vector(23 downto 0);
+        DATAI_IM    : in  std_logic_vector(IFFT_Width-1 downto 0);
+        DATAI_RE    : in  std_logic_vector(IFFT_Width-1 downto 0);
         DATAI_VALID : in  std_logic;
         NGRST       : in  std_logic;
         READ_OUTP   : in  std_logic;
         SLOWCLK     : in  std_logic;
         -- Outputs
         BUF_READY   : out std_logic;
-        DATAO_IM    : out std_logic_vector(23 downto 0);
-        DATAO_RE    : out std_logic_vector(23 downto 0);
+        DATAO_IM    : out std_logic_vector(IFFT_Width-1 downto 0);
+        DATAO_RE    : out std_logic_vector(IFFT_Width-1 downto 0);
         DATAO_VALID : out std_logic;
         OUTP_READY  : out std_logic
     );
@@ -144,16 +146,16 @@ architecture architecture_Acquisition of Acquisition is
     -- Port list
     port(
         --Inputs
-        aimag_i  : in  std_logic_vector(10 downto 0);
-        areal_i  : in  std_logic_vector(10 downto 0);
-        bimag_i  : in  std_logic_vector(10 downto 0);
-        breal_i  : in  std_logic_vector(10 downto 0);
+        aimag_i  : in  std_logic_vector(FFT_Width-1 downto 0);
+        areal_i  : in  std_logic_vector(FFT_Width-1 downto 0);
+        bimag_i  : in  std_logic_vector(FFT_Width-1 downto 0);
+        breal_i  : in  std_logic_vector(FFT_Width-1 downto 0);
         clock_i  : in  std_logic;
         nreset_i : in  std_logic;
         
         --Outputs
-        cimag_o  : out std_logic_vector(21 downto 0);
-        creal_o  : out std_logic_vector(21 downto 0)
+        cimag_o  : out std_logic_vector(IFFT_Width-1 downto 0);
+        creal_o  : out std_logic_vector(IFFT_Width-1 downto 0)
     );
     end component;
     
@@ -176,11 +178,11 @@ architecture architecture_Acquisition of Acquisition is
         data_width : integer := 64
     );
     port(	
-        A:	in std_logic_vector(data_width-1 downto 0);
-        B:	in std_logic_vector(data_width-1 downto 0);
+        A:	in std_logic_vector(DDS_Width-1 downto 0);
+        B:	in std_logic_vector(DDS_Width-1 downto 0);
         Cin:	in std_logic;
 
-        S:	out std_logic_vector(data_width-1 downto 0);
+        S:	out std_logic_vector(DDS_Width-1 downto 0);
         Cout:	out std_logic
     );
     end component;
@@ -218,18 +220,19 @@ begin
     
     -- DDS e contador 
     SINE_GENERATOR: COREDDS_C0 port map (CLK,Frequency_offset_data, '0','1',NRST,'1',cos_signal,open,sin_signal);
-    CONTADOR_ESTADO: contador generic map (10) port map(clk, counter_init, count_state);
+    CONTADOR_ESTADO: contador generic map (Contador_WIDTH) port map(clk, counter_init, count_state);
     
     -- Entrada
-    MULT1: Multiplier_simplified generic map(data_width) port map(cos_signal,MAX_INPUT_I,I1_signal);
-    MULT2: Multiplier_simplified generic map(data_width) port map(sin_signal,MAX_INPUT_I,I2_signal);
-    MULT3: Multiplier_simplified generic map(data_width) port map(sin_signal,MAX_INPUT_Q,Q2_signal);
-    MULT4: Multiplier_simplified generic map(data_width) port map(cos_signal,MAX_INPUT_Q,Q1_signal);
+    MULT1: Multiplier_simplified generic map(DDS_Width) port map(cos_signal,MAX_INPUT_I,I1_signal);
+    MULT2: Multiplier_simplified generic map(DDS_Width) port map(sin_signal,MAX_INPUT_I,I2_signal);
+    MULT3: Multiplier_simplified generic map(DDS_Width) port map(sin_signal,MAX_INPUT_Q,Q2_signal);
+    MULT4: Multiplier_simplified generic map(DDS_Width) port map(cos_signal,MAX_INPUT_Q,Q1_signal);
 	Q2_signal_n <= not(Q2_signal);
-    SUM_I: UAL generic map(data_width) port map(I1_signal,Q2_signal_n,'1',
-                                FFT_I_signal(data_width downto 0),FFT_I_signal(23));
-    SUM_Q: UAL generic map(data_width) port map(I2_signal,Q1_signal,'0',
-                                FFT_Q_signal(data_width downto 0),FFT_Q_signal(23));
+    
+    SUM_I: UAL generic map(SUM_Width) port map(I1_signal,Q2_signal_n,'1',
+                                FFT_I_signal(SUM_Width-1 downto 0),FFT_I_signal(SUM_Width));
+    SUM_Q: UAL generic map(SUM_Width) port map(I2_signal,Q1_signal,'0',
+                                FFT_Q_signal(SUM_Width-1 downto 0),FFT_Q_signal(SUM_Width));
     
     -- Código CA
 	CA_CODE: L1_CA_generator 
@@ -244,7 +247,7 @@ begin
 	     SAT        => SAT_int
 	   );
     FFT_CA_in_real(0) <= '1';
-    FFT_CA_in_real(data_width downto 1) <= (others => CA_PRN);
+    FFT_CA_in_real(FFT_Width-1 downto 1) <= (others => CA_PRN);
     
     --FFT
     FFT_IQ : COREFFT_C2
@@ -279,6 +282,7 @@ begin
 	    OUTP_READY  => open
 	);
     
+    
     -- Correlação
     MULT5: complex_multiplier_C0 port map (FFT_X_signal, FFT_Y_signal, CA_CONJ_out_imag, FFT_CA_out_real, 
                                             CLK, NRST, IFFT_in_imag, IFFT_in_real); -- Verificar
@@ -298,9 +302,9 @@ begin
 	    OUTP_READY  => open
 	);
     
-    CA_CONJ_out_imag <= std_logic_vector(resize(-signed(FFT_CA_out_imag), 10));
-    SAT_int <= to_integer(unsigned(count_state(9 downto 5)));
-    Frequency_offset_data <= count_state(4 downto 0);
+    CA_CONJ_out_imag <= std_logic_vector(resize(-signed(FFT_CA_out_imag), FFT_Width));
+    SAT_int <= to_integer(unsigned(count_state(Contador_WIDTH-1 downto Contador_WIDTH-5)));
+    Frequency_offset_data <= count_state(Contador_WIDTH-6 downto 0);
     NRST <= not (RST);
 
 end architecture_Acquisition;
