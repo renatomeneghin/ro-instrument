fe8f4a538c74a429e160773d71b584cd21dce6f0
renatomeneghin <142346494+renatomeneghin@users.noreply.github.com>
2025-12-24 13:28:08 -0300

    Update Phase_Discriminator.vhd

--- Implementacao_teste_GNSS/hdl/Tracking/Phase_Discriminator.vhd
+++ Implementacao_teste_GNSS/hdl/Tracking/Phase_Discriminator.vhd
@@ -1,31 +1,89 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;   
 use ieee.std_logic_arith.all;
-use ieee.math_real.all;
 
-entity Phase_Discriminator is
-	generic(
-		Discriminator_Type : natural := 1 
-	);
-	Port (
-		I_signal 	: in  real;
-		Q_signal	: in  real;
-		discriminator   : out real
-	);
-end Code_Discriminator;
+entity PLL_Discriminator is
+    generic (
+        acc_width : integer := 32;
+        DISC_TYPE : integer := 0   -- 0 = Costas, 1 = atan (future)
+    );
+    port (
+        IP : in std_logic_vector(acc_width-1 downto 0);
+        QP : in std_logic_vector(acc_width-1 downto 0);
 
-architecture Behavioral of Code_Discriminator is
-	
+        PLL_err : out std_logic_vector(acc_width-1 downto 0)
+    );
+end entity;
+
+architecture structural of PLL_Discriminator is
+
+    component Negative_Integer is
+        generic ( data_width : integer := 32 );
+        port (
+            SIG_IN  : in  std_logic_vector(data_width-1 downto 0);
+            SIG_OUT : out std_logic_vector(data_width-1 downto 0)
+        );
+    end component;
+
+    signal Q_neg : std_logic_vector(acc_width-1 downto 0);
+
 begin
-	Discriminator_1: if Discriminator_Type = 1 generate
-		discriminator <= sign(I_signal)*Q_signal;
-	end generate Discriminator_1;
 
-	Discriminator_2: if Discriminator_Type = 2 generate
-		discriminator <= I_signal*Q_signal;
-	end generate Discriminator_2;
+    ------------------------------------------------------------------
+    -- Costas discriminator (default)
+    ------------------------------------------------------------------
+    gen_costas : if DISC_TYPE = 0 generate
 
-	Discriminator_3: if Discriminator_Type = 3 generate
-		discriminator <= atan2(Q_signal,I_signal);
-	end generate Discriminator_3;
-end Behavioral;
+        NEG_Q : Negative_Integer
+            generic map (data_width => acc_width)
+            port map (
+                SIG_IN  => QP,
+                SIG_OUT => Q_neg
+            );
+
+        PLL_err <= QP when IP(acc_width-1) = '0' else Q_neg;
+
+    end generate;
+
+    ------------------------------------------------------------------
+    -- Placeholder for atan-based discriminator
+    ------------------------------------------------------------------
+    gen_atan : if DISC_TYPE = 1 generate
+        -- Not recommended for ASIC / space
+        PLL_err <= (others => '0');
+    end generate;
+
+end architecture;
+
+
+--library IEEE;
+--use IEEE.STD_LOGIC_1164.ALL;   
+--use ieee.std_logic_arith.all;
+--use ieee.math_real.all;
+--
+--entity Phase_Discriminator is
+	--generic(
+		--Discriminator_Type : natural := 1 
+	--);
+	--Port (
+		--I_signal 	: in  real;
+		--Q_signal	: in  real;
+		--discriminator   : out real
+	--);
+--end Code_Discriminator;
+--
+--architecture Behavioral of Code_Discriminator is
+	--
+--begin
+	--Discriminator_1: if Discriminator_Type = 1 generate
+		--discriminator <= sign(I_signal)*Q_signal;
+	--end generate Discriminator_1;
+--
+	--Discriminator_2: if Discriminator_Type = 2 generate
+		--discriminator <= I_signal*Q_signal;
+	--end generate Discriminator_2;
+--
+	--Discriminator_3: if Discriminator_Type = 3 generate
+		--discriminator <= atan2(Q_signal,I_signal);
+	--end generate Discriminator_3;
+--end Behavioral;
