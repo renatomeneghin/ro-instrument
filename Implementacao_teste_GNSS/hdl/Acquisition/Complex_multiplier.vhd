134cb3928e07cb25b93f8d715a9706ca82ca772f
BeagleBoard User <user@example.com>
2026-05-25 16:46:09 -0300

    falta implementar um registrador na saída e testar

--- Implementacao_teste_GNSS/hdl/Acquisition/Complex_multiplier.vhd
+++ Implementacao_teste_GNSS/hdl/Acquisition/Complex_multiplier.vhd
@@ -20,7 +20,6 @@ library IEEE;
 
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.all;
 
 entity Complex_multiplier is
 generic(
@@ -42,53 +41,50 @@ port (
 end Complex_multiplier;
 
 architecture architecture_Complex_multiplier of Complex_multiplier is
-
-	signal ab, aibi, aib, abi, p, pi, aibi_invertido : std_logic_vector(2*data_width-1 downto 0);
-    signal p, pi : std_logic_vector(2*data_width downto 0);
-    signal OverFlow, OverFlow_Sub, OverFlow_Soma : std_logic;
-    signal cout, couti : std_logic; --Sem uso
-
-    component UAL is
-    generic(
-        data_width : integer := 33
-    );
-    port(
-        A : in std_logic_vector(data_width-1 downto 0);
-        B : in std_logic_vector(data_width-1 downto 0);
-        Cin : in std_logic;
-
-        S :	out std_logic_vector(data_width-1 downto 0);
-        Cout : out std_logic
-    );
-    end component;
     
-    component Multiplier_simplified is
-    generic(
-        data_width : integer := 12
-    );
-    port(
-        A :	in std_logic_vector(data_width-1 downto 0);
-        B :	in std_logic_vector(1 downto 0);
-        S :	out std_logic_vector(data_width downto 0)
-    );
-    end component;
+    signal a, b, ai, bi: signed(data_width -1 downto 0)
+	signal ab, aibi, aib, abi : signed(2*data_width-1 downto 0);
+    --signal OverFlow, OverFlow_Sub, OverFlow_Soma : std_logic;
+    --signal cout, couti : std_logic; --Sem uso
+
+    --component UAL is
+    --generic(
+        --data_width : integer := 33
+    --);
+    --port(
+        --A : in std_logic_vector(data_width-1 downto 0);
+        --B : in std_logic_vector(data_width-1 downto 0);
+        --Cin : in std_logic;
+
+        --S :	out std_logic_vector(data_width-1 downto 0);
+        --Cout : out std_logic
+    --);
+    --end component;
     
 begin
-    aibi_invertido <= not(aibi);
+
+    a <= signed(a_real);
+    ai <= signed(a_imag);
+    b <= signed(b_real);
+    bi <= signed(b_imag);
+    
+    ab <= a*b;
+    abi <= a*bi;
+    aib <= ai*b;
+    aibi <= ai*bi;
+    
+    p_real <= (ab(2*data_width-1) & ab) - (aibi(2*data_width-1) & aibi);
+    p_imag <= (aib(2*data_width-1) & aib) + (abi(2*data_width-1) & abi);
     
     --port map da Subtracao a_real*b_real - a_imag*b_imag Com Extencao de sinal (evitar OV)
-    Sub: UAL port map (ab(2*data_width-1) & ab, aibi_invertido(2*data_width-1) & aibi, "1", p, cout);
+    --Sub: UAL port map (ab(2*data_width-1) & ab, aibi_invertido(2*data_width-1) & aibi_invertido, "1", p, cout);
     --port map da Soma a_imag*b_real + a_real*b_imag Com Extencao de sinal
-    Sum: UAL port map(aib(2*data_width-1) & aib, abi(2*data_width-1) & abi, "0", pi, couti);
-    
-    
+    --Sum: UAL port map(aib(2*data_width-1) & aib, abi(2*data_width-1) & abi, "0", pi, couti);
     
     --deteccao de OverFlow
-    OverFlow_Sub <= ((ab(2*data_width-1) xnor aibi_invertido(2*data_width-1)) and (ab(2*data_width-1) xor p(2*data_width)));
-    OverFlow_Soma <= ((aib(2*data_width-1) xnor abi(2*data_width-1)) and (aib(2*data_width-1) xor pi(2*data_width)));
-    OverFlow <= OverFlow_Sub or OverFlow_Soma;
+    --OverFlow_Sub <= ((ab(2*data_width-1) xnor aibi_invertido(2*data_width-1)) and (ab(2*data_width-1) xor p(2*data_width)));
+    --OverFlow_Soma <= ((aib(2*data_width-1) xnor abi(2*data_width-1)) and (aib(2*data_width-1) xor pi(2*data_width)));
+    --OverFlow <= OverFlow_Sub or OverFlow_Soma;
     -- atribuicao de Saidas p_real e p_imag
-    p_real <= p;
-    p_imag <= pi;
     
 end architecture_Complex_multiplier;
